158c069821ca84da64ffb18e9077af5bf1726587
CamaladdinAhmadzada <ecemaleddin49@gmail.com>
2023-08-22 19:33:49 +0400

    idk

--- main.js
+++ main.js
@@ -61,3 +61,16 @@ document.addEventListener("DOMContentLoaded", () => {
   slider();
   mobileMenu();
 });
+
+
+$(document).ready(function() {
+  var targetDateString = "2023-12-31T04:33:30"; 
+  var targetDate = new Date(targetDateString);
+
+  $(".count-down").countdown(targetDate, function(event) {
+      $(this).find(".count-day .number").text(event.strftime('%D'));
+      $(this).find(".count-hour .number").text(event.strftime('%H'));
+      $(this).find(".count-min .number").text(event.strftime('%M'));
+      $(this).find(".count-sec .number").text(event.strftime('%S'));
+  });
+});
